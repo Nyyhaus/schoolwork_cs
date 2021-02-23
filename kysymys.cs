76f2429387bb2bce39eb942b1c1faedd7b3a70f5
Ville Uusitalo <75932298+Nyyhaus@users.noreply.github.com>
2021-02-23 22:12:53 +0200

    Create kysymys.cs

---
+++ kysymys.cs
@@ -0,0 +1,13 @@
+using System;
+
+class hedelma
+{
+	static void Main()
+	{
+		string vastaus;
+		
+		Console.Write("Mikä on lempihedelmäsi? ");
+		vastaus = Console.ReadLine();
+		Console.WriteLine("Lempihedelmäsi on " + vastaus);
+	}
+}
