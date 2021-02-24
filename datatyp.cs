d41f49eff6484d74b1796518fbe17829b443a7c7
Ville Uusitalo <75932298+Nyyhaus@users.noreply.github.com>
2021-02-24 12:03:27 +0200

    Create datatyp.cs

---
+++ datatyp.cs
@@ -0,0 +1,15 @@
+		short var1;
+		byte  var2;
+		float var3;
+		char var4;
+		Boolean var5;
+		ulong var6;
+		decimal var7;
+		var1 = -100;
+		var2 = 20;
+		var3 = 1.50f;
+		var4 = 'g';
+		var5 = false;
+		var6 = 2156703459ul;
+		var7 = 7.2233490653m;
+		
